09330333cb54d46be34752ac3bbac202ea56718e
Olga Lorenz <ol900ivas@gmail.com>
2023-05-16 20:09:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 // import { PersistGate } from 'redux-persist/integration/react';
 // import { persistor, store } from './redux/store';
